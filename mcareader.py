42293baf92735cd5a44172b1b01a3f30e17eae36
Guillermo Hernández <guillehg@gmail.com>
2017-01-16 16:50:11 +0100

    Bump version number to 0.2.0

--- mcareader.py
+++ mcareader.py
@@ -12,7 +12,7 @@ from scipy.stats import linregress
 from io import open  # python 2 compatibility
 
 __author__ = 'Dih5'
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 
 def _str_to_array(s, sep=" "):
--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
+    version='0.2.0',
 
     description="A python interface to Amptek's mca files",
     long_description=long_description,
@@ -78,7 +78,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    #packages=find_packages(exclude=['docs', 'tests', 'demos']),
+    # packages=find_packages(exclude=['docs', 'tests', 'demos']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
@@ -114,7 +114,6 @@ setup(
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     # data_files=[('my_data', ['data/data_file'])],
     data_files=[],
-
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
